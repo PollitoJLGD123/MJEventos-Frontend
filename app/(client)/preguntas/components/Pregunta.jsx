162b50f63acc12f91a860be9d54774b2b2a953e6
PedroDavidDD <u19207574@utp.edu.pe>
2025-03-28 11:02:53 -0500

    styled(Pregunta): Respuestas (css)
    - Se cambia el formato del grosor de la pregunta y la selección de las mismas.
    - Se cambia la respuestas a las preguntas

--- app/(client)/preguntas/components/Pregunta.jsx
+++ app/(client)/preguntas/components/Pregunta.jsx
@@ -7,12 +7,12 @@ export default function Pregunta({ question, answer }) {
   return (
     <article className="border-l-4 pl-4 border-solid border-[#ea9d2f]">
       <h3 
-        className="font-bold text-[25px] cursor-pointer hover:text-[#ea9d2f]" 
+        className="font-medium text-[25px] cursor-pointer hover:text-[#ea9d2f] text-black select-none" 
         onClick={() => setIsOpen(!isOpen)}
       >
         {question}
       </h3>
-      {isOpen && <p className="text-[18px] mt-4">{answer}</p>}
+      {isOpen && <p className="text-[18px] mt-4 italic text-black">{answer}</p>}
     </article>
   );
 }
