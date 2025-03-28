c02c5a2755f15006a6a87182133e52bdd9c05ecd
PedroDavidDD <u19207574@utp.edu.pe>
2025-03-28 11:09:37 -0500

    add: 'PedroDavid'

--- app/(client)/preguntas/components/Pregunta.jsx
+++ app/(client)/preguntas/components/Pregunta.jsx
@@ -5,14 +5,14 @@ export default function Pregunta({ question, answer }) {
   const [isOpen, setIsOpen] = useState(false);
 
   return (
-    <article className="border-l-4 pl-4 border-solid border-[#5c1787]">
+    <article className="border-l-4 pl-4 border-solid border-[#ea9d2f]">
       <h3 
-        className="font-bold text-[25px] cursor-pointer hover:text-[#5c1787]" 
+        className="font-medium text-[25px] cursor-pointer hover:text-[#ea9d2f] text-black select-none" 
         onClick={() => setIsOpen(!isOpen)}
       >
         {question}
       </h3>
-      {isOpen && <p className="text-[18px] mt-4">{answer}</p>}
+      {isOpen && <p className="text-[18px] mt-4 italic text-black">{answer}</p>}
     </article>
   );
 }
--- app/(client)/preguntas/components/Principal.jsx
+++ app/(client)/preguntas/components/Principal.jsx
@@ -5,12 +5,12 @@ export default function Principal() {
         <>
             <section className="relative w-full h-[500px]  overflow-hidden">
                 <img
-                    src="/faq/fondo.webp"
+                    src="/faq/fondo.jpg"
                     alt="logo de preguntas y respuestas"
                     className="absolute  inset-0 w-full h-full xl:h-auto  object-cover z-0 "
                 />
-                <div className="absolute inset-0 z-2 flex justify-center items-center  bg-gradient-to-r from-[#7b22b344] to-[#7b22b344]">
-                    <h1 className="text-white text-[30px] text-center md:text-[50px] font-semibold" >PREGUNTAS FRECUENTES</h1>
+                <div className="absolute inset-0 z-2 flex justify-center items-center  bg-gradient-to-r from-[#ffffff00] to-[#0000006b]">
+                    <h1 className="text-white text-[30px] text-center md:text-[50px] font-semibold drop-shadow-lg shadow-black" >PREGUNTAS FRECUENTES</h1>
                 </div>
             </section>
         </>
